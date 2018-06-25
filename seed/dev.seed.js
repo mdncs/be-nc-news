db6448524344766e8fdd31fc7d63359171d806fb
mdncs <madalinamaria.bumb@gmail.com>
2018-06-25 18:00:03 +0100

    fixed config error

--- seed/dev.seed.js
+++ seed/dev.seed.js
@@ -2,7 +2,7 @@ const seedDB = require('./seed');
 const mongoose = require('mongoose');
 const { topicData, userData, articleData } = require('./devData');
 const { generateRandomCommentData } = require('../utils');
-const path = require('./config');
+const path = require('../config');
 
 const commentData = generateRandomCommentData(articleData, userData);
 
